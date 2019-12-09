0ba1b698e55371a92685a51ab798f0ca4574bd41
Viacheslav Gordeiko <slav78rus@gmail.com>
2019-12-09 22:46:02 +0300

    Refactor small

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,6 +1,7 @@
+import { cons } from 'hexlet-pairs';
+
 import gameEngine from '..';
 import { getRandomInt, toString } from '../utils';
-import { cons } from 'hexlet-pairs';
 
 const description = 'What is the result of the expression?';
 
